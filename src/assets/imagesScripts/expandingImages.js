693e52d5da7c16a56455b054b6acb7e6bf739f30
EricChukoltyansky <kazohelmet@gmail.com>
2022-05-10 20:43:38 +0300

    art work grid

--- src/assets/imagesScripts/expandingImages.js
+++ src/assets/imagesScripts/expandingImages.js
@@ -4,38 +4,148 @@ import david from "../../assets/images/david-kracov.jpeg";
 import dorit from "../../assets/images/dorit-levinstein.jpeg";
 import gal from "../../assets/images/gal-yosef.jpeg";
 import kobra from "../../assets/images/kobra.jpeg";
+import yoel from "../../assets/images/yoel.jpg";
+import daniel from "../../assets/images/daniel-gastaud.jpg";
+import calman from "../../assets/images/calman-shemi.jpg";
+import fg from "../../assets/images/fg.jpg";
+import fred from "../../assets/images/fred-allard.jpg";
+import jonas from "../../assets/images/jonas-leriche.jpg";
+import jonty from "../../assets/images/jonty.jpg";
+import kunst from "../../assets/images/kunst.jpg";
+import marco from "../../assets/images/marco-battaglini.jpg";
+import sibuet from "../../assets/images/sibuet.jpg";
+import sn from "../../assets/images/sn.jpg";
+import metis from "../../assets/images/metis-atash.jpg";
+import plum from "../../assets/images/plum.jpg";
+import randy from "../../assets/images/randy-cooper.jpg";
 
 const expandingImages = [
-  {
-    id: 1,
-    src: alec,
-    alt: "alec",
-  },
-  {
-    id: 2,
-    src: angelo,
-    alt: "angelo",
-  },
-  {
-    id: 3,
-    src: david,
-    alt: "david",
-  },
-  {
-    id: 4,
-    src: dorit,
-    alt: "dorit",
-  },
-  {
-    id: 5,
-    src: gal,
-    alt: "gal",
-  },
-  {
-    id: 6,
-    src: kobra,
-    alt: "kobra",
-  },
+  [
+    {
+      id: 1,
+      src: alec,
+      alt: "alec",
+      title: "Alec Monopoly",
+    },
+    {
+      id: 2,
+      src: angelo,
+      alt: "angelo",
+      title: "Angelo Accardi",
+    },
+    {
+      id: 3,
+      src: david,
+      alt: "david",
+      title: "David Kracov",
+    },
+    {
+      id: 4,
+      src: dorit,
+      alt: "dorit",
+      title: "Dorit Levinstein",
+    },
+    {
+      id: 5,
+      src: gal,
+      alt: "gal",
+      title: "Gal Yosef",
+    },
+    {
+      id: 6,
+      src: kobra,
+      alt: "kobra",
+      title: "Eduardo Kobra",
+    },
+    {
+      id: 7,
+      src: yoel,
+      alt: "yoel",
+      title: "Yoel Benharrouche",
+    },
+  ],
+  [
+    {
+      id: 8,
+      src: sn,
+      alt: "sn",
+      title: "Sn",
+    },
+    {
+      id: 9,
+      src: calman,
+      alt: "calman",
+      title: "Calman Shemi",
+    },
+    {
+      id: 10,
+      src: fg,
+      alt: "fg",
+      title: "F&G",
+    },
+    {
+      id: 12,
+      src: fred,
+      alt: "fred",
+      title: "Fred Allard",
+    },
+    {
+      id: 11,
+      src: plum,
+      alt: "plum",
+      title: "Plum",
+    },
+    {
+      id: 7,
+      src: metis,
+      alt: "metis",
+      title: "Metis Atash",
+    },
+    {
+      id: 13,
+      src: randy,
+      alt: "randy",
+      title: "Randy Cooper",
+    },
+  ],
+  [
+    {
+      id: 15,
+      src: daniel,
+      alt: "daniel",
+      title: "Daniel Gastaud",
+    },
+    {
+      id: 16,
+      src: marco,
+      alt: "marco",
+      title: "Marco Battaglini",
+    },
+    {
+      id: 17,
+      src: jonty,
+      alt: "jonty",
+      title: "Jonty Hurwitz",
+    },
+    {
+      id: 21,
+      src: kunst,
+      alt: "kunst",
+      title: "Kunst Met Een R",
+    },
+    {
+      id: 19,
+      src: sibuet,
+      alt: "sibuet",
+      title: "Paul Sibuet",
+    },
+    {
+      id: 18,
+      src: jonas,
+      alt: "jonas",
+      title: "Jonas Leriche",
+    },
+  ],
 ];
 
 export default expandingImages;
--- src/components/ExpandingSlider/ExpandingSlider.jsx
+++ src/components/ExpandingSlider/ExpandingSlider.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import images from "../../assets/imagesScripts/expandingImages";
 import "./ExpandingSlider.scss";
 
 function ExpandingSlider() {
+  const [value, setValue] = useState(0);
+
   return (
     <>
+      {console.log(value)}
       <div className="expanding-wrapper">
         <div class="row">
-          {images.map((image, index) => (
-            <div class="sec">
+          {images[value].map((image, index) => (
+            <div className="sec">
               <div
                 key={index}
                 className="frame"
                 style={{ backgroundImage: `url(${image.src})` }}
-              ></div>
+              >
+                <p>{image.title}</p>
+              </div>
             </div>
+          ))}
+        </div>
+        <div className="toggle-btns">
+          {Array.from({ length: 3 }).map((item, index) => (
+            <>
+              <div
+                key={index}
+                className={value === index ? "toggle-btn active" : "toggle-btn"}
+                onClick={() => setValue((prevState) => (prevState = index))}
+              ></div>
+            </>
           ))}
         </div>
       </div>
