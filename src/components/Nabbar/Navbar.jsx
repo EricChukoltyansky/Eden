a14eabad6b99c8418343adf724e762c4bbf3d0ec
EricChukoltyansky <kazohelmet@gmail.com>
2022-05-10 11:57:30 +0300

    navbar

--- src/components/Nabbar/Navbar.jsx
+++ src/components/Nabbar/Navbar.jsx
@@ -1,5 +1,37 @@
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/EricChukoltyansky/Eden.git
-// git push -u origin main
+import React from "react";
+import "./Navbar.scss";
+import { AiOutlineHeart } from "react-icons/ai";
+function Navbar() {
+  return (
+    <>
+      <header>
+        <nav className="navbar">
+          <div className="top-nav">
+            <input
+              type="text"
+              placeholder="Search Artist, Artwork, Gallery "
+            ></input>
+            <span className="heart-symbol">
+              <AiOutlineHeart />
+            </span>
+            <span className="online-shop">Online Shop</span>
+          </div>
+          <div className="bottom-nav">
+            <ul>
+              <li>Home</li>
+              <li>Galleries</li>
+              <li>Artists</li>
+              <li>Collection</li>
+              <li>Artworks</li>
+              <li>News</li>
+              <li>About Eden</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Navbar;
