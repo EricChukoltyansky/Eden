2c6c2f80c1022be904ff35fa0f940a92c8a8d46f
EricChukoltyansky <kazohelmet@gmail.com>
2022-05-12 10:36:36 +0300

    fixed collections

--- src/components/Collections/Collections.jsx
+++ src/components/Collections/Collections.jsx
@@ -1,48 +1,85 @@
-import React from "react";
-// import images from "../../assets/imagesScripts/collectionsImages";
+import React, { useEffect, useState } from "react";
+import images from "../../assets/imagesScripts/images";
 import "./Collections.scss";
 
 function Collections() {
+  const [value, setValue] = useState(0);
+
+  console.log(value);
+
+  const increment = () => {
+    setValue((prevState) => (prevState += 1));
+    if (value >= images.length - 1) setValue((prevState) => (prevState = 0));
+  };
+
+  useEffect(() => {
+    const loop = setInterval(() => {
+      increment();
+    }, 3000);
+    return () => clearInterval(loop);
+  }, [value]);
+
   return (
     <>
-      {/* <div className="collections-container">
-        <h3>Collections</h3>
-        <div className="collections-wrapper">
-          {images.map((image, index) => (
-            <div key={index} className="collection-item">
-              <img src={image.src} alt={image.alt} />
-              <div className="collection-title">
-                <h2>{image.title}</h2>
-                <h4>{image.subtitle}</h4>
-                <button>{image.button}</button>
-              </div>
-            </div>
-          ))}
-        </div>
-        <button className="explore-btn">Explore all Collections</button>
-      </div> */}
-
       <div className="collection-container">
         <h3>Collections</h3>
         <div className="collection-wrapper">
           <div className="left-img coll-img">
-            <h2>Painting</h2>
-            <h4>
-              Explore Eden Gallery's collection of acrylic, oil and graffiti
-              paintings
-            </h4>
-            <button>Explore</button>
+            <div className="content-wrapper">
+              <h2>Painting</h2>
+              <h4>
+                Explore Eden Gallery's collection of acrylic, oil and graffiti
+                paintings
+              </h4>
+              <button>Explore</button>
+            </div>
           </div>
           <div className="right-img coll-img">
-            <h2>Sculptures</h2>
-            <h4>
-              Explore Eden Gallery's collection of unique free-standing
-              sculptures
-            </h4>
-            <button>Explore</button>
+            <div className="content-wrapper">
+              <h2>Sculptures</h2>
+              <h4>
+                Explore Eden Gallery's collection of unique free-standing
+                sculptures
+              </h4>
+              <button>Explore</button>
+            </div>
           </div>
         </div>
         <button className="explore-btn">Explore all Collections</button>
+      </div>
+
+      <div className="slider-responsive-container">
+        <h3>Collections</h3>
+        <p className="discover-paragraph">Discover our latest collections</p>
+        <div className="slider">
+          <div className="image-container">
+            <div className="img-titles">
+              <div className="title-wrapper">
+                <h2>{images[value].title}</h2>
+                <h4>{images[value].subtitle}</h4>
+                <p>{images[value].description}</p>
+              </div>
+              <button>{images[value].button}</button>
+            </div>
+            <img src={images[value].src} alt={images[value].alt} />
+          </div>
+        </div>
+        <div className="bottom-slider">
+          <span className="bottom-title">{images[value].alt}</span>
+        </div>
+        <div className="sliding-btns">
+          {Array.from({ length: 5 }).map((item, index) => (
+            <>
+              <div
+                key={index}
+                className={
+                  value === index ? "sliding-btn active" : "sliding-btn"
+                }
+                onClick={() => setValue(index)}
+              ></div>
+            </>
+          ))}
+        </div>
       </div>
     </>
   );
